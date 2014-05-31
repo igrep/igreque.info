e10f42076cbd06d3ea0b8920ebd474f46a8fd6bf
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2014-05-31 16:35:11 +0900

    configure hakyll to serve image

--- main.hs
+++ main.hs
@@ -20,6 +20,7 @@ main = hakyll $ do
   tags <- buildTags "posts/*" (fromCapture "tags/*.html")
   cssRules              -- Compressed CSS
   idRules "fonts/*"     -- Fonts
+  idRules "imgs/*"      -- Images
   postRules tags        -- Render posts
   postsListRules        -- Render posts list
   indexRules tags       -- Index
