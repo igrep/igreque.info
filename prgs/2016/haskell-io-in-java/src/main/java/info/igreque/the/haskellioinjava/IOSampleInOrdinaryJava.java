52f520c344a553c90b9b3c5738816963b8cb1cc9
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2016-11-05 11:05:50 +0900

    Forgot to complete IOSampleInOrdinaryJava

--- prgs/2016/haskell-io-in-java/src/main/java/info/igreque/the/haskellioinjava/IOSampleInOrdinaryJava.java
+++ prgs/2016/haskell-io-in-java/src/main/java/info/igreque/the/haskellioinjava/IOSampleInOrdinaryJava.java
@@ -1,8 +1,16 @@
 package info.igreque.the.haskellioinjava;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class IOSampleInOrdinaryJava {
   public static void main(String args[]) throws Exception {
-    IO.runMain(IOSample.main);
+    System.out.println("Nice to meet you!");
+    System.out.println("May I have your name? ");
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    String name = reader.readLine();
+    System.out.println("Your name is " + name + "?");
+    System.out.println("Nice name!");
   }
 }
 
